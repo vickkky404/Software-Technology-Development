40a3ccc36c33cf44cf9aacbfc480b9c77757b00f
vickkky404 <nalinikantbastia@gmail.com>
2026-09-14 00:58:07 +0530

    datatypes in JavaScript

---
+++ JS/Foundation/datatypes.js
@@ -0,0 +1,47 @@
+console.log("Hello");
+// console.log("Chai")   this is the comment in the code in between the code..
+
+
+/*
+This is multiline comment 
+
+
+*/
+
+
+
+
+/*
+
+Types of Data Types in Javascript
+
+String
+Number
+Boolean
+BigInt
+
+Undefined
+null (this is atually null)
+0 (this is not null)
+
+Object
+Symbol
+
+*/
+
+
+
+
+var score = 102 //old way of declaring variable
+
+let score = 102 // this is the new way of declaring the variable in the javaScript
+
+let name = "chaicode.com"
+let isLoggedIn = false
+
+// object
+let teaTypes = ["Green Tea", "Black Tea", "Oolong Tea", "White Tea"] // this is like an array
+let user = { name: "Chai", age: 25, city: "New York" } // this is like an object
+
+
+let getScore = score;
