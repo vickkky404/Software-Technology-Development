e41f9d04e29c7d99c887a1cdb31d83879cc43140
vickkky404 <nalinikantbastia@gmail.com>
2026-09-14 04:39:06 +0530

    types

---
+++ JS/Foundation/operator.js
@@ -0,0 +1,17 @@
+let score = 102
+let bonus = 25
+
+let totalScore = score + bonus
+console.log(totalScore) // this will print the value of the variable totalScore.
+
+
+
+//comparision operation
+
+let num1 = 3
+let num2 = 3
+let num3 = 6
+
+
+console.log(num1 == num2) // this will print true because both values are equal
+console.log(num1 == num3) // this will print false because both values are not equal
