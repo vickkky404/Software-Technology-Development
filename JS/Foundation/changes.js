fd60e664c3d4fe94d0e4e56b86ae18bff0cd4bfc
vickkky404 <nalinikantbastia@gmail.com>
2026-09-14 01:03:15 +0530

    datatypes in javascript

---
+++ JS/Foundation/changes.js
@@ -0,0 +1,8 @@
+let gameName = "SpiderMan"
+
+gameName = "BatMan" //the value will get updated
+
+console.log(gameName) // this will print the value of the variable gameName.
+
+
+username = "Vickkky";
