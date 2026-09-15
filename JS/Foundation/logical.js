76b76d1957035b8342644ed4715a48ad75317e5a
vickkky404 <nalinikantbastia@gmail.com>
2026-09-15 13:28:10 +0530

    logic

---
+++ JS/Foundation/logical.js
@@ -0,0 +1,11 @@
+// && -> and
+// || -> or
+// !  -> reverse
+
+
+let isLoggedIn = true
+let ispaid = false
+
+console.log(isLoggedIn && ispaid) // this will print false because both values are not true;
+console.log(isLoggedIn || ispaid) // this will print true because at least one value is true;
+console.log(!isLoggedIn) // this will print false because the value is reversed;
