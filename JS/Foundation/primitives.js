5be34307bfb190747bc43af1221e6459150c71ea
vickkky404 <nalinikantbastia@gmail.com>
2026-09-21 17:27:21 +0530

    Primitive and NonPrimitive

---
+++ JS/Foundation/primitives.js
@@ -0,0 +1,17 @@
+// Number
+
+let balance = 120;
+let anotherBalance = new Number(120);
+
+
+
+
+// checking the type of the variable that will explaon the difrence between the usage of new keyword and the plain variabe
+console.log(typeof balance);
+console.log(typeof anotherBalance);
+
+
+console.log(balance);
+
+console.log(anotherBalance);
+console.log(typeof(anotherBalance));
