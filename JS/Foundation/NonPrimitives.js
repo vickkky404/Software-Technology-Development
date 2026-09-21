3ee9c68d0d92106c2a8fa22d0764d556b3f02e35
vickkky404 <nalinikantbastia@gmail.com>
2026-09-21 18:14:33 +0530

    Primitive

---
+++ JS/Foundation/NonPrimitives.js
@@ -0,0 +1,36 @@
+const obj = {
+    firstname :  "hitesh",
+    isLoggedin : true, 
+    age : 22,
+    address : {city : "delhi", state : "delhi"}
+};
+
+//injecting more or new values to the object although it is declared as a constant
+obj.lastname = "Kumar Maharaj"
+
+//printin every values in the object
+console.log(obj);
+
+
+
+console.log(typeof obj);
+console.log(obj);
+
+
+// date
+let today = new Date();
+console.log(today.getDate);
+
+
+// Array
+let heros = ["batman", "Dexter", "Ironman", true];
+
+console.log(heros[0]);
+console.log(heros[1]);
+console.log(heros[2]);
+console.log(heros[3]);
+
+
+
+
+
