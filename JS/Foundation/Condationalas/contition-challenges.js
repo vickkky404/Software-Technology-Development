aedc2deddbe108d432b8e6fb69df7e7a5d2b21df
vickkky404 <nalinikantbastia@gmail.com>
2026-09-22 10:33:25 +0530

    condationals

---
+++ JS/Foundation/Condationalas/contition-challenges.js
@@ -0,0 +1,13 @@
+// check if the number is greater than another
+ 
+let num1 = 5;
+let num2 = 8;
+
+console.log("This is regular code.")
+
+if (num1 > num2){
+    console.log(" num1 is greater than num2");
+}
+
+console.log("Iam regular bottom code")
+
